f8b579669fc13448d7bf3a99e0e30728ea0e900a
pds0309 <ehd0309@gmail.com>
2022-04-19 15:37:00 +0900

    PhoneValidator: add Test
    전화번호 검증에 대한 요구사항은 +82로 시작하며 13글자이어야 하는 것이었다.
    
    그렇지 않은 입력이 들어왔을 경우에 대한 테스트 케이스를 추가로 작성한다.

--- src/test/java/com/prac/softwaretest/utils/PhoneValidatorTest.java
+++ src/test/java/com/prac/softwaretest/utils/PhoneValidatorTest.java
@@ -2,6 +2,7 @@ package com.prac.softwaretest.utils;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,6 +26,32 @@ class PhoneValidatorTest {
 
         // Then
         assertTrue(isValid);
+    }
+
+    @Test
+    @DisplayName("인자로 얻은 전화번호가 13글자 이상일 때는 부적절한 전화번호이다.")
+    void itShouldValidatePhoneNumberHasLengthBiggerThanN() {
+        // Given
+        String phone = "+82106564515951";
+
+        // When
+        boolean isValid = validator.test(phone);
+
+        // Then
+        assertFalse(isValid);
+    }
+
+    @Test
+    @DisplayName("인자로 얻은 전화번호가 +82로 시작하지 않을 때는 부적절한 전화번호이다.")
+    void itShouldValidatePhoneNumberDoesNotStart82() {
+        // Given
+        String phone = "+771065645159";
+
+        // When
+        boolean isValid = validator.test(phone);
+
+        // Then
+        assertFalse(isValid);
     }
 
 }
