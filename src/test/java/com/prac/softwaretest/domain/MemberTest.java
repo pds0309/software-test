182abfb560a0c6fc953f0e0703a8c68095df3a97
pds0309 <ehd0309@gmail.com>
2022-04-19 16:43:29 +0900

    Member test 3 - name 검증 테스트 케이스 추가
    테스트 실페

--- src/test/java/com/prac/softwaretest/domain/MemberTest.java
+++ src/test/java/com/prac/softwaretest/domain/MemberTest.java
@@ -1,9 +1,14 @@
 package com.prac.softwaretest.domain;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,6 +25,10 @@ class MemberTest {
     private static final String SUCCESS_NAME = "김갑환";
     private static final int SUCCESS_AGE = 25;
 
+    private static Stream<String> blankStrings() {
+        return Stream.of("", " ", null);
+    }
+
     @Test
     @DisplayName("멤버 객체를 초기화 할 수 있다.")
     void initMemberTest() {
@@ -29,5 +38,19 @@ class MemberTest {
         assertEquals(SUCCESS_NAME, member.getName());
         assertEquals(SUCCESS_AGE, member.getAge());
     }
+
+    @ParameterizedTest
+    @MethodSource("blankStrings")
+    @ValueSource(strings = "열글자가넘는길고더러운이름이다")
+    @DisplayName("Member의 이름은 null이거나 공백이거나 10글자 초과일 수 없다.")
+    void initMemberFailWithInvalidNameParameter(String name) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Member.builder()
+                    .name(name)
+                    .age(25)
+                    .build();
+        });
+    }
+
 
 }
