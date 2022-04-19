58fc7d8ed3fde49c7a65b7929e0d3c564e91d0e2
pds0309 <ehd0309@gmail.com>
2022-04-19 16:34:48 +0900

    Member test 2
    Member 객체를 생성할 수 있는 최소한의 테스트를 먼저 만들고 최소한으로 통과시킨다.

--- src/main/java/com/prac/softwaretest/domain/Member.java
+++ src/main/java/com/prac/softwaretest/domain/Member.java
@@ -1,5 +1,31 @@
 package com.prac.softwaretest.domain;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Member {
-    
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_NAME")
+    private String name;
+
+    @Column(name = "AGE")
+    private int age;
+
+    @Builder
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
--- src/test/java/com/prac/softwaretest/domain/MemberTest.java
+++ src/test/java/com/prac/softwaretest/domain/MemberTest.java
@@ -1,21 +1,33 @@
 package com.prac.softwaretest.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Long id
- * String name : 이름은 10글자 이하여야 한다.
- * int age : 0 이상 200 미만이어야 한다.
+ * String name : 널일 수 없으며 이름은 10글자 이하여야 한다.
+ * int age : 널일 수 없으며 0 이상 200 미만이어야 한다.
  */
 @DisplayName("Member 엔티티 클래스 테스트")
 class MemberTest {
 
     private Member member;
 
-    @BeforeEach
-    void prepareMember() {
-        member = new Member();
+    private static final String SUCCESS_NAME = "김갑환";
+    private static final int SUCCESS_AGE = 25;
+
+    @Test
+    @DisplayName("멤버 객체를 초기화 할 수 있다.")
+    void initMemberTest() {
+        // given + when
+        member = new Member(SUCCESS_NAME, SUCCESS_AGE);
+        // then
+        assertEquals(SUCCESS_NAME, member.getName());
+        assertEquals(SUCCESS_AGE, member.getAge());
     }
 
 }
