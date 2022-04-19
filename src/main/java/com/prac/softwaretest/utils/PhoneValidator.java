ea9756151efa3d6850504e83166117ff3cb1336d
pds0309 <ehd0309@gmail.com>
2022-04-19 15:21:22 +0900

    PhoneValidator: 2.1 Pass Test

--- src/main/java/com/prac/softwaretest/utils/PhoneValidator.java
+++ src/main/java/com/prac/softwaretest/utils/PhoneValidator.java
@@ -1,5 +1,12 @@
 package com.prac.softwaretest.utils;
 
-public class PhoneValidator {
+import java.util.function.Predicate;
+
+public class PhoneValidator implements Predicate<String> {
+
+    @Override
+    public boolean test(String s) {
+        return false;
+    }
 
 }
