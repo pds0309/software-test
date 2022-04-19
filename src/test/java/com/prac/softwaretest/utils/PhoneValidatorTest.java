425cbfb9c0ddbcc322a537135b3a747a7d6ccd1e
pds0309 <ehd0309@gmail.com>
2022-04-19 15:10:13 +0900

    PhoneValidator : 1 - Fail-Test

---
+++ src/test/java/com/prac/softwaretest/utils/PhoneValidatorTest.java
@@ -0,0 +1,20 @@
+package com.prac.softwaretest.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PhoneValidatorTest {
+
+    private PhoneValidator validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = new PhoneValidator();
+    }
+
+    @Test
+    void itShouldValidatePhoneNumber() {
+
+    }
+
+}
