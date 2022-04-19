bae31f08770a687cedbcd12ad410eefe27246af9
pds0309 <ehd0309@gmail.com>
2022-04-19 16:49:20 +0900

    Member test 4 - name 검증 실제 코드 작성
    실제 name 검증 코드를 엔티티 클래스에 작성하여 테스트를 통과시킨다.

--- src/main/java/com/prac/softwaretest/domain/Member.java
+++ src/main/java/com/prac/softwaretest/domain/Member.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
 
 import javax.persistence.*;
 
@@ -17,7 +18,7 @@ public class Member {
     @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_NAME")
+    @Column(name = "MEMBER_NAME", nullable = false, length = 10)
     private String name;
 
     @Column(name = "AGE")
@@ -25,7 +26,17 @@ public class Member {
 
     @Builder
     public Member(String name, int age) {
+        validName(name);
         this.name = name;
         this.age = age;
+    }
+
+    private void validName(String name) {
+
+        Assert.notNull(name, "이름은 NULL일 수 없습니다");
+
+        if ("".equals(name.trim()) || name.length() > 10) {
+            throw new IllegalArgumentException("이름은 공백이거나 10글자를 넘을 수 없다");
+        }
     }
 }
