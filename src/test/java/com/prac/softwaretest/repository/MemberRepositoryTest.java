39098757292926ca97ba925a507c741bb115e708
pds0309 <ehd0309@gmail.com>
2022-04-21 09:50:36 +0900

    MemberRepository save Test 2
    다른 스타일의 테스트

--- src/test/java/com/prac/softwaretest/repository/MemberRepositoryTest.java
+++ src/test/java/com/prac/softwaretest/repository/MemberRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -39,6 +40,24 @@ class MemberRepositoryTest {
         assertNotNull(savedMember);
         assertEquals(member, savedMember);
 
+    }
+
+    @Test
+    @DisplayName("Member 는 Save 될 수 있다2")
+    void saveMemberTest2() {
+
+        // given
+        Member member = SampleMember.of();
+        // when
+        memberRepository.save(member);
+        // then
+        Optional<Member> maybeMember = memberRepository.findById(member.getId());
+        assertThat(maybeMember)
+                .isPresent()
+                .hasValueSatisfying(m -> {
+                    assertThat(m.getId()).isEqualTo(member.getId());
+                    assertThat(m.getAge()).isEqualTo(member.getAge());
+                });
     }
 
 }
