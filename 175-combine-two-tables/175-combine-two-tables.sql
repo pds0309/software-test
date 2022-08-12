fc7867a0318e5712ad7e685c944aabd483eaf099
pds <76927397+pds0309@users.noreply.github.com>
2022-08-12 18:16:59 +0900

    delete unintentional commit

--- 175-combine-two-tables/175-combine-two-tables.sql
+++
@@ -1,5 +0,0 @@
-/* Write your PL/SQL query statement below */
-SELECT FIRSTNAME AS "firstName" , LASTNAME as "lastName" , CITY as "city", STATE as "state"
-FROM PERSON P 
-LEFT OUTER JOIN ADDRESS A
-ON P.PERSONID = A.PERSONID
